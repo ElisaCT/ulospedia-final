f7d9ac02e829ce39e6ccc384911072e03797207c
ElisaCT <elisact02@gmail.com>
2023-04-27 11:50:25 +0700

    home

---
+++ src/components/RagamUlos.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class=" bg-[#FDF7E9] pt-[61px] pb-[105px] mt-24 px-[180px]">
+        <!-- Title -->
+        <div class="font-[#0A0A0A] pb-8">
+            <h1 class="text-[44px] font-bold">Ragam Ulos</h1>
+            <p class="text-xl">Temukan berbagai keunikan ulos dari beberapa etnis Batak berikut.</p>
+        </div>
+
+        <!-- Button -->
+        <div class="gap-6 font-[#0A0A0A] text-2xl text-center">
+            <!-- first col -->
+            <div class="grid grid-cols-3 gap-6 pb-6"> 
+                <a class="bg-[#BBC6E6] col-span-2 h-[140px] rounded-[20px]  flex justify-center items-center" href=#>Batak Toba</a>
+                <a class="bg-[#FBEFD2] h-[140px] rounded-[20px]  flex justify-center items-center" href=#>Batak Simalungun</a>
+            </div>
+
+            <!-- second col -->
+            <div class="grid grid-cols-3 gap-6 text-2xl h-[140px]">
+                <a class="bg-[#FBEFD2] rounded-[20px]  flex justify-center items-center" href=#>Batak Mandailing</a>
+                <a class="bg-[#BBC6E6] rounded-[20px]  flex justify-center items-center" href=#>Batak Karo</a>
+                <a class="bg-[#FBEFD2] rounded-[20px]  flex justify-center items-center" href=#>Batak Angkola</a>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
