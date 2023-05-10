0fa7262b0e9a1d534563792abe01fb48122a3339
ElisaCT <elisact02@gmail.com>
2023-05-10 16:29:11 +0700

    load more

--- src/views/PenenunGedogan.vue
+++ src/views/PenenunGedogan.vue
@@ -17,7 +17,7 @@
                   class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-neutral_30"
                 >
                   <div class="h-[300px] w-[252px]">
-              <div class="gradient"></div>
+                    <div class="gradient"></div>
                     <img
                       class="h-full w-full object-cover transition-transform rounded-lg"
                       :src="penenun.imageUrl"
@@ -26,7 +26,7 @@
                   <div
                     class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-neutral_100 group-hover:from_neutral_80 group-hover:via-neutral_60 group-hover:to-neutral_80"
                   ></div>
-                  <div class="absolute inset-0 flex translate-y-[80%] flex-col text-left pl-6">
+                  <div class="absolute inset-0 flex translate-y-[80%] flex-col text-left pl-6 z-[3]">
                     <h1 class="text-xl font-medium text-neutral_10">{{ penenun.name }}</h1>
                     <p class="text-neutral_10">{{ penenun.age }}</p>
                   </div>
@@ -34,13 +34,18 @@
               </router-link>
             </div>
           </div>
-    </div>
+        </div>
       
-
       <div v-else>
         <CardSkeleton />
       </div>
     
+      
+    </div>
+
+    <!-- Load more -->
+    <div class="max-w-6xl mx-auto text-center mb-10 lg:mb-14">
+        <button class="text-neutral_70 font-medium text-lg bg-neutral_20 items-center px-4 py-4 rounded-lg w-full max-w-md" @click="loadMore" v-if="!lastPage">Muat lebih banyak</button>
     </div>
   
 </template>
@@ -54,25 +59,47 @@ export default {
   name: 'App',
   data: function () {
     return {
-      penenuns: null
+      penenuns: null,
+      pageNo: 1,
+      lastPage:true
     }
   },
   mounted() {
     axios
-      .get('http://company.ditenun.com/api/v1/ulospedia/client/weavers?theLoom=Gedongan&pageNo=1')
+      .get(
+        `http://company.ditenun.com/api/v1/ulospedia/client/weavers?theLoom=Gedongan&pageNo=${this.pageNo}`
+      )
       .then((response) => {
+        console.log(response.data)
         this.penenuns = response.data.data.weaverList.clientWeaverResponseList
-        console.log(this.penenuns)
+
+        // cek state apakah akan menjadi page terakhir atau tidak
+        if (!response.data.data.weaverList.isLastPage) {
+          this.pageNo = this.pageNo + 1
+          this.lastPage = false
+        }
       })
       .catch((error) => {
         console.log(error)
       })
   },
-  methods:{
-    // eslint-disable-next-line no-unused-vars
-    // goToDetail(id){
-    //   this.$router.push('/penenun-gedogan/${id}')
-    // }
+  methods: {
+    async loadMore() {
+      const morePenenuns = await axios.get(
+        `http://company.ditenun.com/api/v1/ulospedia/client/weavers?theLoom=Gedongan&pageNo=${this.pageNo}`
+      )
+      console.log(morePenenuns.data)
+      this.penenuns = this.penenuns.concat(
+        morePenenuns.data.data.weaverList.clientWeaverResponseList
+      )
+      // cek state apakah akan menjadi page terakhir atau tidak
+      if (!morePenenuns.data.data.weaverList.isLastPage) {
+        this.pageNo = this.pageNo + 1
+        this.lastPage = false
+      } else {
+        this.lastPage = true
+      }
+    }
   },
   components: { CardSkeleton }
 }
@@ -85,5 +112,6 @@ export default {
     position: absolute;
     background-image: linear-gradient(to top, rgba(0, 0, 0, 0.375) 0.79%, rgba(0, 0, 0, 0.175) 17.08%, rgba(0, 0, 0, 0.1) 31.87%, rgba(0, 0, 0, 0) 99.17%);
     border-radius: 8px;
+    z-index: 2;
   }
 </style>
