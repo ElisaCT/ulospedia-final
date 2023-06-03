d2bc4b370f137d7ac21d03fbc26473492c674f5d
rewinapakpahan <rewinap31@gmail.com>
2023-06-03 15:12:46 +0700

    update verifikasi home

--- cypress/e2e/01-UI-Automation/02-End User/02-A-Category-Partitioning-Method/01-VerifikasiHomePage.cy.js
+++ cypress/e2e/01-UI-Automation/02-End User/02-A-Category-Partitioning-Method/01-VerifikasiHomePage.cy.js
@@ -7,11 +7,11 @@ describe('Verifikasi Home Page - End User', () => {
 
 
     it('Verfikasi button Eksplor ulos dapat digunakan', () => {
-        cy.getById('btn-exploreUlos').click()      //menekan tombol explore ulos
-        cy.getById('field-cariUlos').should('exist')  //verifikasi sudah dihalaman galeri ulos
+        cy.getById('btn-exploreUlos').click() //menekan tombol explore ulos
+        cy.getById('field-cariUlos').should('exist') //verifikasi sudah dihalaman galeri ulos
     })
 
-    it.only('Verfikasi section jelajahi Ulos', () => {
+    it('Verfikasi section jelajahi Ulos', () => {
         cy.getById('span-btn-jelajahiUlos').click()
         cy.getByTestDataId('tab-btn-sejarah-ulos').should('be.visible').should('exist');
         cy.getById('text-sejarahUlos').should('be.visible').should('contain', 'Sejarah Ulos');
@@ -19,19 +19,44 @@ describe('Verifikasi Home Page - End User', () => {
     })
 
     it('Verifikasi section Apa itu Ulos', () => {
+        // Memastikan teks judul ada dan sesuai
+        cy.get('#card-apa-itu-ulos h4').should('have.text', 'Apa itu ulos?');
 
+        // Memastikan tombol jelajahi ulos mengarahkan ke router dan halaman yang benar
+        cy.get('#span-btn-jelajahiUlos').click();
+        cy.url().should('eq', 'http://127.0.0.1:1753/galeri-ulos');
+
+        cy.get('#field-cariUlos').should('be.visible');
     })
 
     it('Verifikasi section Lahir hidup dan Mati', () => {
+        // Memastikan konten pada elemen "Lahir, hidup, dan mati" sesuai dengan yang diharapkan
+        cy.get('#card-lahir-hidup-mati')
+            .find('h4')
+            .should('have.text', 'Lahir, hidup, dan mati');
+        cy.get('#span-btn-sejarahUlos').click();
+        cy.url().should('eq', 'http://127.0.0.1:1753/sejarah-ulos');
 
+        // Memastikan tombol sejarah ulos aktif atau terlihat oleh pengguna
+        cy.get('[data-test-id="btn-sejarahUlos"]').should('be.visible');
+
+        cy.getById('text-sejarahUlos').should('be.visible').should('contain', 'Sejarah Ulos');
+        // Memastikan ada gambar pada elemen
+        cy.get('#gambar-sejarah-ulos').should('exist');
     })
 
     it('Verifikasi section Fakta Menarik Ulos', () => {
-
+        // Click on the "next-button"
+        cy.get('#text-fakta-menarik').should('have.text', 'Temukan Fakta Menarik Ulos');
+        cy.get('#next-button').should('be.enabled')
+        cy.get('#prev-button').should('be.enabled')
     })
 
-    it('Verifikasi section Ragam ulos: Batak Toba', () => {
-
+    it.only('Verifikasi section Ragam ulos: Batak Toba', () => {
+        //cy.get('#btn-ragamUlos-BatakToba').click()
+        cy.get('#btn-ragamUlos-BatakSimalungun').click( )
+        // cy.get('#navbar-GaleriUlos').click() 
+        cy.get('.text-sm').should('have.text', 'Batak Toba');
     })
 
     it('Verifikasi section Ragam ulos: Batak Simalungun', () => {
